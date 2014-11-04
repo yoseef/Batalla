b9149dbb28899ad3abee45541d85e92c8b064c45
yoseef <yoseef@gmail.com>
2014-11-04 11:54:16 +0100

    modificat el metode formar() afegit el metode changedireccio()

--- src/net/Batalla2/Exercit.java
+++ src/net/Batalla2/Exercit.java
@@ -14,43 +14,65 @@ import java.util.Random;
  */
 public class Exercit {
 
-    ArrayList<Soldat> exercit;
-    Random r;
-    int files;
-    double canvasWidth;   
-    private double puntInicial;
+    /**
+     * Guarda tots el soldats.
+     */
+    private final ArrayList<Soldat> exercit;
+    /**
+     * crea enters aleatoris.
+     */
+    private final Random r;
+    /**
+     * necessitem calcluar la mida del camp per poder controlar els soldats.
+     */
+    private final double canvasWidth;
+    /**
+     * indica si el soldat ha arribat el desti.
+     */
+    boolean hanArribat;
 
-    //constructor
-    public Exercit(double widthCanvas) {
+    /**
+     *
+     * @param midaCamp la mida del camp
+     */
+    public Exercit(final double midaCamp) {
         exercit = new ArrayList<>();
-        r = new Random();        
-        canvasWidth = widthCanvas;        
-        puntInicial = 0;
+        r = new Random();
+        canvasWidth = midaCamp;
+        hanArribat = true;
     }
 
-    //getters:
-    
-    public Soldat getObtenirSoldat(int pos) {
+    /**
+     *
+     * @param pos si volem un soldat especific apartir de la posicio
+     * @return retorna el soldat de la posicio
+     */
+    public final Soldat getObtenirSoldat(final int pos) {
         return exercit.get(pos);
-    }   
-    
-    public ArrayList<Soldat> getSoldats() {
+    }
+
+    /**
+     *
+     * @return la array amb tots els soldats.
+     */
+    public final ArrayList<Soldat> getSoldats() {
         return exercit;
     }
 
-    //setter    
-
-    //metodes de classe
-    public void afegirSoldat(Soldat nouSoldat) {
+    /**
+     *
+     * @param nouSoldat va afegint soldats a l'array
+     */
+    public final void afegirSoldat(final Soldat nouSoldat) {
         exercit.add(nouSoldat);
     }
 
     /**
-     * @param numFiles que hihaura
-     * @param pI inidica quina pos estaran dreta o esquerra
+     *
+     * @param numFiles fer la formacio apartir del numero de files
      */
-    public void formarExercit(int pI, int numFiles) {
-  
+    public final void formarExercit(final int numFiles) {
+
         int[] files = new int[numFiles];
         for (int i = 0; i < exercit.size(); i++) {
             int numAleat = r.nextInt(files.length);
@@ -60,13 +82,14 @@ public class Exercit {
         for (int i = 0; i < files.length; i++) {
             for (int j = 0; j < files[i]; j++) {
                 if (count < exercit.size()) {
-                    double h = i * exercit.get(count).getHeight() + i * 15;
+                    double h = i * exercit.get(count).getHeight() + i * N15;
                     double w;
-                    if (pI > 0) {
+                    if (exercit.get(count).getbandol() > 0) {
                         w = exercit.get(count).getWidth() * j;
                     } else {
-                        puntInicial = canvasWidth - exercit.get(count).getWidth();
-                        w = puntInicial - (exercit.get(count).getWidth() * j);
+                        double formEsque = canvasWidth
+                                - exercit.get(count).getWidth();
+                        w = formEsque - (exercit.get(count).getWidth() * j);
                     }
                     exercit.get(count).formar(w, h);
                     count++;
@@ -74,24 +97,27 @@ public class Exercit {
             }
         }
     }
-
+    /**
+     * numero 15.
+     */
+    private static final int N15 = 15;
     /**
      *
-     * @param direccioX
+     * @return false si tots els soldats encara es poden moure i true si jan han
+     * arribat i no poden moure.
      */
-    boolean hanArribat = true;
-    public boolean atacar(int direccioX) {
+    public final boolean atacar() {
         double desti;
         int totalMoguts = 0;
         for (int i = 0; i < exercit.size(); i++) {
-            if (direccioX > 0) {
+            if (exercit.get(i).getbandol() > 0) {
                 //dreta
                 desti = canvasWidth - exercit.get(i).getWidth();
             } else {
-                //esquerra 
+                //esquerra
                 desti = 0;
             }
-            totalMoguts += exercit.get(i).moure(direccioX, desti);
+            totalMoguts += exercit.get(i).moure(desti);
             if (totalMoguts == 0) {
                 hanArribat = true;
             } else {
@@ -101,14 +127,30 @@ public class Exercit {
         return hanArribat;
     }
 
-    public void comprovarMorts(Exercit exercitOponent) {
+    /**
+     *
+     * @param exOponent el seguent exercit amb el que es compara si han
+     * xucat
+     */
+    public final void comprovarMorts(final Exercit exOponent) {
         for (int i = 0; i < exercit.size(); i++) {
-            for (int j = 0; j < exercitOponent.getSoldats().size(); j++) {
-                if (exercit.get(i).intersecta(exercitOponent.getSoldats().get(j))) {
-                    exercitOponent.getObtenirSoldat(j).borrarImatge();
-                    exercitOponent.getSoldats().remove(j);
+            for (int j = 0; j < exOponent.getSoldats().size(); j++) {
+                if (exercit.get(i).intersecta(exOponent.getSoldats().get(j))) {
+                    exOponent.getObtenirSoldat(j).setImg("exp.png");
+                    exOponent.getObtenirSoldat(j).borrarImatge();
+                    exOponent.getSoldats().remove(j);
                 }
             }
+        }
+    }
+
+    /**
+     * canvia la direccio de al soldat perque vagi en sentit contrari.
+     */
+    public final void changeDireccio() {
+        for (Soldat sldt : exercit) {
+            int tmp = sldt.getbandol() * -1;
+            sldt.setbandol(tmp);
         }
     }
 }
